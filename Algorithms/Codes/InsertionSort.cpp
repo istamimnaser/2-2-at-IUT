3bf5902815f38c243d9a580e4e3313cf73f9c3eb
Tamim Mahrush Naser <itstamimnaser@gmail.com>
2026-05-30 14:11:15 +0600

    day1

---
+++ Algorithms/Codes/InsertionSort.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void insertionSort(vector<int>&arr){
+    int n = arr.size();
+
+    for(int i = 1; i < n; i++){
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// time complexity: O(n^2) in worst case and O(n) in best case
+// space complexity: O(1)
